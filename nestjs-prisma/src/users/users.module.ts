cb6b49029505c327d0f6e83f18a3f05d39e97eaf
onozaty <onozaty@gmail.com>
2024-02-13 23:56:59 +0900

    [NestJS + Prisma] importのパスが誤っていたので修正(srcからのパスになっており、テスト時には解決できなかった)

--- nestjs-prisma/src/users/users.module.ts
+++ nestjs-prisma/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from './../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
--- nestjs-prisma/src/users/users.service.ts
+++ nestjs-prisma/src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from './../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserDto } from './dto/user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
