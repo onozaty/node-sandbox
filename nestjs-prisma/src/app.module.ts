692f49e0cdad4223c40722ac91d9e0759dec526f
onozaty <onozaty@gmail.com>
2024-03-17 21:44:36 +0900

    [NestJS + Prisma] AppControllerなど不要なものを削除

--- nestjs-prisma/src/app.module.ts
+++ nestjs-prisma/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import * as Joi from 'joi';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { UsersModule } from './users/users.module';
@@ -23,9 +21,7 @@ import { UsersModule } from './users/users.module';
       }),
     }),
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
