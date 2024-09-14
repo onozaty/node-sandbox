ab1df89b8bb6d57e8cc82e22574bd94fa4a90e12
onozaty <onozaty@gmail.com>
2024-09-14 09:09:11 +0900

    [vitest-monorepo] 問題再現のために、構成変更

--- vitest-monorepo/b/src/sum2.test.ts
+++ vitest-monorepo/b/src/sum2.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
-import { sum } from './sum.js'
+import { sum } from './sum2'
 
-test('adds 1 + 2 to equal 3', () => {
+test('b test 1', () => {
   expect(sum(1, 2)).toBe(3)
 })
